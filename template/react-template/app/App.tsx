b9591930e58fc2ea8bef1b0bba6541120176ca98
kxh4892636 <kxh4892636@gmail.com>
2026-01-16 20:05:35 +0800

    feat: react template

---
+++ template/react-template/app/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+export function App() {
+	return (
+		<div className="flex h-screen w-screen flex-col items-center justify-center bg-red-50">
+			<div className="text-4xl text-blue-500">Hello World!</div>
+		</div>
+	);
+}
+
+export default App;
