945add643192dc369b76976cf60158c7707154e5
kxh4892636 <kxh4892636@gmail.com>
2026-01-16 21:19:56 +0800

    feat: hono template

--- templates/react-template/app/App.tsx
+++ templates/react-template/app/App.tsx
@@ -1,9 +1,11 @@
 import "./index.css";
+import { Button } from "antd";
 
 export function App() {
 	return (
 		<div className="flex h-screen w-screen flex-col items-center justify-center bg-red-50">
 			<div className="text-4xl text-blue-500">Hello World!</div>
+			<Button>Click me</Button>
 		</div>
 	);
 }
